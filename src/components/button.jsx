0c03719b92eab41881a2ebd1e876eb64dd065e7a
Lennart <lennartjvw@icloud.com>
2019-11-11 12:03:52 +0100

    Added header banner

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 class Button extends Component {
   render() {
-    // ...
+    return (
+        <a href="#" className={this.props.class}>
+          <span>
+            {this.props.text}
+          </span>
+        </a>
+    )
   }
 }
 export default Button;
--- src/containers/HeadBanner.jsx
+++ src/containers/HeadBanner.jsx
@@ -1,12 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Button from '../components/button';
+import logo from '../assets/logo.png';
 
 class HeadBanner extends Component {
-  render() {
-    return (
-      <div className="HeadBanner">
-      </div>
-    )
-  };
+    render() {
+        return (
+            <div className="HeadBanner">
+                <div className="row">
+                    <div className="inner row">
+                        <div className="col-md-6 col-sm-12">
+                            <div className="holder">
+                                <h1>We are Corner Office Games</h1>
+                                <p className="subtext">We are Corner Office Games and we make games We are Corner Office
+                                    Games and we make
+                                    games </p>
+                                <Button class="button primary" text="Read more"></Button>
+                                {/*<Button class="button secondary" text="Check out"></Button>*/}
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-sm-12">
+                            <div className="img-holder">
+                                <img src={logo} alt=""/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    };
 }
 
 export default HeadBanner;
