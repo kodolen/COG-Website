8efd2c22f1f02cfff096620effd1871230217304
Lennart <lennartjvw@icloud.com>
2019-11-12 20:54:10 +0100

    save commit

--- src/containers/Feeds.jsx
+++ src/containers/Feeds.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
 class Feeds extends Component {
+  componentDidMount() {
+    
+    fetch('https://api.apify.com/v2/datasets/MNGp3TA6yZuNKRij8/items')
+        .then(results => {
+          return results.json();
+        }).then(data => {
+
+    });
+  }
+
   render() {
     return (
       <div className="Feeds">
+
       </div>
     )
   };
